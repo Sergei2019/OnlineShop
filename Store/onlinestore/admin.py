0743141594dfa69fd50e2bcc4c9eacd2d261a854
Сергей Соболев <serg2010sob@gmail.com>
2022-12-18 00:17:42 +0300

    добавил модели в админку

--- Store/onlinestore/admin.py
+++ Store/onlinestore/admin.py
@@ -1,3 +1,57 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product, Order, OrderItem, ShippingAdress
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name',
+                    'price',
+                    'colors',
+                    'sizes',
+                    )
+    list_editable = ('price', 'colors', 'sizes', )
+    search_fields = ('name', 'price', 'colors', 'sizes', )
+    list_filter = ('name', 'price', 'colors', 'sizes', )
+    empty_value_display = '-пусто-'
+
+
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('product',
+                    'order',
+                    'quantity',
+                    'date_added',
+                    )
+    list_editable = ('order', 'quantity', )
+    search_fields = ('product', 'order', 'quantity', 'date_added', )
+    list_filter = ('product', 'order', 'quantity', 'date_added', )
+    empty_value_display = '-пусто-'
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('customer',
+                    'data_order',
+                    'complete',
+                    'transaction_id',
+                    )
+    search_fields = ('customer', 'data_order', 'complete', 'transaction_id', )
+    list_filter = ('customer', 'data_order', 'complete', 'transaction_id', )
+    empty_value_display = '-пусто-'
+
+
+class ShippingAdressAdmin(admin.ModelAdmin):
+    list_display = ('customer',
+                    'order',
+                    'state',
+                    'city',
+                    'adress',
+                    'date_added'
+                    )
+    search_fields = ('customer', 'order', 'state', 'city', 'adress', 'date_added')
+    list_filter = ('customer', 'order', 'state', 'city', 'adress', 'date_added')
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderItem, OrderItemAdmin)
+admin.site.register(ShippingAdress, ShippingAdressAdmin)
--- Store/onlinestore/models.py
+++ Store/onlinestore/models.py
@@ -1,3 +1,88 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, 
+                                on_delete=models.CASCADE,
+                                null=True,
+                                blank=True)
+    name = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    COLOR = [
+        ('BLACK', 'Тёмный'),
+        ('WHITE', 'Светлый'),
+        ('RED', 'Красный'),
+        ('BLUE', 'Голубой/Синий'),
+        ('GREEN', 'Зелёный'),
+    ]
+    SIZE = [
+        ('XS', 'XS'),
+        ('S', 'S'),
+        ('M', 'M'),
+        ('L', 'L'),
+        ('XL', 'XL'),
+    ]
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField()
+    colors = models.CharField(default='BLACK',
+                              max_length=20,
+                              choices=COLOR)
+    sizes = models.CharField(default='XS',
+                             max_length=2,
+                             choices=SIZE)
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer,
+                                 on_delete=models.SET_NULL,
+                                 null=True,
+                                 blank=True)
+    data_order = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False)
+    transaction_id = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product,
+                                on_delete=models.SET_NULL,
+                                blank=True,
+                                null=True)
+    order = models.ForeignKey(Order,
+                              on_delete=models.SET_NULL,
+                              blank=True,
+                              null=True)
+    quantity = models.IntegerField(default=0,
+                                   blank=True,
+                                   null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+
+class ShippingAdress(models.Model):
+    customer = models.ForeignKey(Customer,
+                                 on_delete=models.SET_NULL,
+                                 null=True,
+                                 blank=True)
+    order = models.ForeignKey(Order,
+                              on_delete=models.SET_NULL,
+                              blank=True,
+                              null=True)
+    state = models.CharField(max_length=200, null=True)
+    city = models.CharField(max_length=200, null=True)
+    adress = models.CharField(max_length=200, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return (self.city, self.adress)
